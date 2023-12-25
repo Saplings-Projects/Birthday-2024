7816417790d4d916dfbb7e1c2a2a98737ffaf452
BishopJohnson <BishopJohnson@users.noreply.github.com>
2023-12-24 20:13:45 -0800

    Added initial settings window

---
+++ Birthday-2024-Project/Scripts/UI/Settings/SettingsAudioTab.gd
@@ -0,0 +1,19 @@
+class_name SettingsAudioTab
+extends TabBar
+
+
+func on_fauna_slider_value_changed(value: float):
+	print("Fauna volume: ", value, " (not implemented)")
+
+
+func on_master_slider_value_changed(value: float):
+	print("Master volume: ", value, " (not implemented)")
+
+
+func on_music_slider_value_changed(value: float):
+	print("Music volume: ", value, " (not implemented)")
+
+
+func on_sfx_slider_value_changed(value: float):
+	print("SFX volume: ", value, " (not implemented)")
+
