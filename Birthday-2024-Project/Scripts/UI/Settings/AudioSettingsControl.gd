a651728426a570230ab36fa2f2ed4425bf3cc4a1
BishopJohnson <BishopJohnson@users.noreply.github.com>
2024-01-02 22:39:26 -0800

    Initial implementation of audio sliders for settings menu

--- Birthday-2024-Project/Scripts/UI/Settings/AudioSettingsControl.gd
+++ Birthday-2024-Project/Scripts/UI/Settings/AudioSettingsControl.gd
@@ -2,18 +2,47 @@ class_name AudioSettingsControl
 extends Control
 
 
+@export_group("Audio Players")
+@export var music_sample_player: AudioStreamPlayer2D # TODO: DELETE WHEN NO LONGER NEEDED
+@export var sfx_sample_player: AudioStreamPlayer2D
+@export var fauna_sample_player: AudioStreamPlayer2D
+
+@export_group("Audio Samples")
+@export var sfx_samples: AudioSamples
+@export var fauna_samples: AudioSamples
+@export var tone_sample: AudioStream # TODO: DELETE WHEN NO LONGER NEEDED
+
+
 func on_fauna_slider_value_changed(value: float):
-	print("Fauna volume: ", value, " (not implemented)")
+	_change_bus_volume(value, "Fauna")
+	_play_sample(fauna_samples.get_random_sample(), fauna_sample_player)
 
 
 func on_master_slider_value_changed(value: float):
-	print("Master volume: ", value, " (not implemented)")
+	_change_bus_volume(value, "Master")
 
 
 func on_music_slider_value_changed(value: float):
-	print("Music volume: ", value, " (not implemented)")
+	_change_bus_volume(value, "Music")
+	_play_sample(tone_sample, music_sample_player) # TODO: Remove when BGM is added.
 
 
 func on_sfx_slider_value_changed(value: float):
-	print("SFX volume: ", value, " (not implemented)")
+	_change_bus_volume(value, "SFX")
+	_play_sample(sfx_samples.get_random_sample(), sfx_sample_player)
+
+
+func _change_bus_volume(value: float, bus_name: String):
+	var db = log(clamp(value, 0.001, 1)) / log(10) * 20 # Convert to decibels
+	var bus_idx = AudioServer.get_bus_index(bus_name)
+	
+	AudioServer.set_bus_volume_db(bus_idx, db)
+
+
+func _play_sample(stream: AudioStream, player: AudioStreamPlayer2D):
+	if stream == null or player.playing:
+		return
+	
+	player.stream = stream
+	player.play()
 
