0a5a0edc78224169271f397cdbda241aeff2b019
BishopJohnson <BishopJohnson@users.noreply.github.com>
2024-01-07 21:34:04 -0800

    - Added `VideoPreferences` script - Added `video` var to `UserPreferences`

--- Birthday-2024-Project/Scripts/Model/Prefs/UserPreferences.gd
+++ Birthday-2024-Project/Scripts/Model/Prefs/UserPreferences.gd
@@ -3,16 +3,19 @@ extends Preferences
 
 
 var audio: AudioPreferences
+var video: VideoPreferences
 
 
 #region Preferences
 
 func _apply_config(config: ConfigFile):
 	audio = AudioPreferences.new(config)
+	video = VideoPreferences.new(config)
 
 
 func _apply_defaults():
 	audio = AudioPreferences.new()
+	video = VideoPreferences.new()
 
 
 #endregion Preferences
---
+++ Birthday-2024-Project/Scripts/Model/Prefs/VideoPreferences.gd
@@ -0,0 +1,41 @@
+class_name VideoPreferences
+extends Preferences
+
+
+var height: int
+var width: int
+var window: WindowType
+
+const _DEFAULT_HEIGHT: int = 1080
+const _DEFAULT_WIDTH: int = 1920
+const _DEFAULT_WINDOW: WindowType = WindowType.FULLSCREEN
+const _SECTION_NAME: String = "video"
+
+
+#region Preferences
+
+func _apply_config(config: ConfigFile):
+	if not config.has_section(_SECTION_NAME):
+		_apply_defaults()
+		return
+	
+	width = config.get_value(_SECTION_NAME, "width", _DEFAULT_WIDTH)
+	height = config.get_value(_SECTION_NAME, "height", _DEFAULT_HEIGHT)
+	window = config.get_value(_SECTION_NAME, "window", _DEFAULT_WINDOW)
+
+
+func _apply_defaults():
+	width = _DEFAULT_WIDTH
+	height = _DEFAULT_HEIGHT
+	window = _DEFAULT_WINDOW
+
+
+#endregion Preferences
+
+enum WindowType
+{
+	FULLSCREEN = 0,
+	WINDOWED = 1,
+	WINDOWED_BORDERLESS = 2,
+}
+
