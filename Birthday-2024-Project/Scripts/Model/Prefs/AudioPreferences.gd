3ed5b73cb22ef4d5eb85f32d54a168d06ee78a9b
BishopJohnson <BishopJohnson@users.noreply.github.com>
2024-01-07 21:39:04 -0800

    Changed naming convention of constants in `AudioPreferences` from lower snake case to upper snake case

--- Birthday-2024-Project/Scripts/Model/Prefs/AudioPreferences.gd
+++ Birthday-2024-Project/Scripts/Model/Prefs/AudioPreferences.gd
@@ -7,31 +7,31 @@ var master: float
 var music: float
 var sfx: float
 
-const _default_fauna: float = 0.5
-const _default_master: float = 1.0
-const _default_music: float = 0.5
-const _default_sfx: float = 0.5
-const _section_name: String = "audio"
+const _DEFAULT_FAUNA: float = 0.5
+const _DEFAULT_MASTER: float = 1.0
+const _DEFAULT_MUSIC: float = 0.5
+const _DEFAULT_SFX: float = 0.5
+const _SECTION_NAME: String = "audio"
 
 
 #region Preferences
 
 func _apply_config(config: ConfigFile):
-	if not config.has_section(_section_name):
+	if not config.has_section(_SECTION_NAME):
 		_apply_defaults()
 		return
 	
-	master = config.get_value(_section_name, "master", _default_master)
-	music = config.get_value(_section_name, "music", _default_music)
-	sfx = config.get_value(_section_name, "sfx", _default_sfx)
-	fauna = config.get_value(_section_name, "fauna", _default_fauna)
+	master = config.get_value(_SECTION_NAME, "master", _DEFAULT_MASTER)
+	music = config.get_value(_SECTION_NAME, "music", _DEFAULT_MUSIC)
+	sfx = config.get_value(_SECTION_NAME, "sfx", _DEFAULT_SFX)
+	fauna = config.get_value(_SECTION_NAME, "fauna", _DEFAULT_FAUNA)
 
 
 func _apply_defaults():
-	master = _default_master
-	music = _default_music
-	sfx = _default_sfx
-	fauna = _default_fauna
+	master = _DEFAULT_MASTER
+	music = _DEFAULT_MUSIC
+	sfx = _DEFAULT_SFX
+	fauna = _DEFAULT_FAUNA
 
 
 #endregion Preferences
