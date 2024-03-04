d2500318b41bdb9414f58368cb6ddba17bdf3a72
Mikotey <mikotey@gmail.com>
2024-03-04 10:24:08 -0800

    Adding button SFX

---
+++ Birthday-2024-Project/Scripts/UI/UISoundPlayer.gd
@@ -0,0 +1,33 @@
+extends Node
+
+func _ready():
+	# when _ready is called, there might already be nodes in the tree, so connect all existing buttons
+	connect_buttons(get_tree().root)
+	get_tree().connect("node_added", _on_SceneTree_node_added)
+
+func _on_SceneTree_node_added(node):
+	if node is BaseButton:
+		connect_to_button(node)
+	elif node is TabContainer:
+		connect_to_tab(node)
+
+func _on_Button_pressed():
+	$ButtonSound.play()
+	
+func _on_Tab_pressed(index : int):
+	$ButtonSound.play()
+
+# recursively connect all buttons
+func connect_buttons(root):
+	for child in root.get_children():
+		if child is BaseButton:
+			connect_to_button(child)
+		elif child is TabContainer:
+			connect_to_tab(child)
+		connect_buttons(child)
+
+func connect_to_button(button):
+	button.connect("pressed", _on_Button_pressed)
+
+func connect_to_tab(tab):
+	tab.connect("tab_clicked", _on_Tab_pressed)
