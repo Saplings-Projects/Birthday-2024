72cb582e1ee6287038f35345ff534c17241cf240
Mikotey <mikotey@gmail.com>
2024-03-17 12:11:46 -0700

    Button Lifted animations; fix for static captures when screen is resized

--- Birthday-2024-Project/Scripts/UI/StaticControlNode.gd
+++ Birthday-2024-Project/Scripts/UI/StaticControlNode.gd
@@ -1,13 +1,21 @@
 extends Control
 class_name StaticControlNode
 
+var _myParent : Control
 var _startingPosition : Vector2
 
-func _ready():
-	var parent : Control = get_parent_control()
-	size = parent.size
+func _initialize():
+	size = _myParent.size
 	position = Vector2.ZERO
 	_startingPosition = global_position
 
+func _ready():
+	_myParent = get_parent_control()
+	_initialize()
+	get_tree().get_root().size_changed.connect(_initialize)
+
 func _process(delta):
 	global_position = _startingPosition
+	
+func _exit_tree():
+	get_tree().get_root().size_changed.disconnect(_initialize)
