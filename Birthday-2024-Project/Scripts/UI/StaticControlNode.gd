a9e5850ec0c92483a2c4d95c7e882292ac33211a
Mikotey <mikotey@gmail.com>
2024-03-20 13:36:42 -0700

    Setting button colliders to update slowly but lock in place if moused over

--- Birthday-2024-Project/Scripts/UI/StaticControlNode.gd
+++ Birthday-2024-Project/Scripts/UI/StaticControlNode.gd
@@ -1,21 +1,53 @@
 extends Control
 class_name StaticControlNode
 
+@export var updateDelay : float = 0.5
+
 var _myParent : Control
-var _startingPosition : Vector2
+var _timer : float
+var _mousedOver : bool
+var _lastPosition : Vector2
+var _lastRotation : float
+
+func _onMouseEnter():
+	_mousedOver = true
+
+func _onMouseExit():
+	_mousedOver = false
 
 func _initialize():
 	size = _myParent.size
 	position = Vector2.ZERO
-	_startingPosition = global_position
+	pivot_offset = Vector2(size.x * 0.5, size.y * 0.5)
+	_timer = updateDelay
+	_lastPosition = _myParent.position
+	_lastRotation = _myParent.rotation
 
 func _ready():
 	_myParent = get_parent_control()
+	set_anchors_preset(Control.PRESET_CENTER)
+	_timer = updateDelay;
+	_mousedOver = false
+	mouse_entered.connect(_onMouseEnter)
+	mouse_exited.connect(_onMouseExit)
+	
 	_initialize()
 	get_tree().get_root().size_changed.connect(_initialize)
 
 func _process(delta):
-	global_position = _startingPosition
+	_timer -= delta
+	if _mousedOver:
+		_timer = updateDelay
+	if _timer < 0:
+		_initialize()
+		
+	if _myParent.rotation != _lastRotation:
+		rotation = -_myParent.rotation
+		_initialize()
+	
+	var moveDifference : Vector2 = _myParent.position - _lastPosition
+	position -= moveDifference
+	_lastPosition = _myParent.position
 	
 func _exit_tree():
 	get_tree().get_root().size_changed.disconnect(_initialize)
